5d517b6944b45b7b8240d33e1ef38b17108dd731
Mark Lamneck <mark.lamneck@mpic.de>
2024-02-14 19:49:27 +0100

    - addes some rules - added vite-plugin-singlefile

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: [],
+  plugins: [
+    "react"
+  ],
   rules: {
+    "react/jsx-key": "warn",
+    "no-prototype-builtins": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "prefer-const": "warn",
+    "@typescript-eslint/no-this-alias": "off",
+    "no-case-declarations": "warn",
+    "no-empty": "off"
   },
 }
